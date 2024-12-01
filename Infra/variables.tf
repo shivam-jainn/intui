e43c92ba4d1efcf09b904e771c3c3bd16e791835
shivam-jainn <shivam.clgstash@gmail.com>
2024-12-01 21:40:02 +0530

    feat(Infra): :bricks: Initialised GCP provider and setup variables.tf
    nada
    
    nada

---
+++ Infra/variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+    type = string
+    description = "GCP projects value"
+    default = "intui-443016"
+}
+
+
+variable "region" {
+    type = map(string)
+    description = "Different regional mapping"
+    default = {
+      "us-central" = "us-central1",
+      "us-east1" = "us-east1"
+    }
+}
+
+variable "zone" {
+    type = map(string)
+    description = "Different regional mapping"
+    default = {
+      "us-central1-a" = "us-central1-a",
+      "us-central1-b" = "us-central1-b",
+      "us-central1-c" = "us-central1-c",
+      "us-east1-a" = "us-east1-a",
+      "us-east1-b" = "us-east1-b",
+      "us-east1-c" = "us-east1-c",
+    }
+}
