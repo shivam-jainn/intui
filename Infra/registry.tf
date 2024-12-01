34f14b2e42e603556e42ec5128f0483f7876db75
shivam-jainn <shivam.clgstash@gmail.com>
2024-12-01 22:13:57 +0530

    chore(Infra): :bricks: replaced hardcoded value of repo id to var

--- Infra/registry.tf
+++ Infra/registry.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "executor-repo" {
   location      = var.region["us-east1"]
-  repository_id = "my-repository"
+  repository_id = var.artifact_repository_id
   description   = "executor docker repository"
   format        = "DOCKER"
 
