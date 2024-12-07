9483db9b1b1f416c5112b725e85925794f2180ff
shivam-jainn <shivam.clgstash@gmail.com>
2024-12-07 18:29:48 +0530

    fix(lib/storage): :construction: removed dependency of code editor and quesiton description components from each other

--- app/(playground)/[questionid]/page.tsx
+++ app/(playground)/[questionid]/page.tsx
@@ -2,39 +2,36 @@
 import React, { useEffect, useState } from "react";
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import QuestionPanel from "@/components/Playground/QuestionPanel";
-import { getDescAndDriver } from "@/lib/common/playground/desc_and_driver";
+import { getDesc } from "@/lib/common/playground/desc_and_driver";
 import CodeEditor from "@/components/Playground/CodeEditor";
 
 export default function Page({ params }: { params: { questionid: string } }) {
   const [questionData, setQuestionData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  console.log("Question Name : ",params.questionid);
-
   const handleCodeChange = (code: string) => {
     console.log('Code changed:', code);
-    // Handle code changes, save to state/database
   };
 
-  useEffect(() => {
-    async function fetchQuestion() {
-      try {
-        setLoading(true);
+  async function fetchQuestion() {
+    try {
+      setLoading(true);
 
-        const response = await fetch(`/api/question/${params.questionid}`);
-        const data = await response.json();
+      const response = await fetch(`/api/question/${params.questionid}`);
+      const data = await response.json();
+      const {question_description} = await getDesc(decodeURIComponent(params.questionid))
+      data['description'] = question_description; 
 
-
-        const {question_description,driver_code} = await getDescAndDriver(params.questionid)
-        data['description'] = question_description.data; 
-        data['driver_code'] = driver_code.data;
-        setQuestionData(data);
-      } catch (error) {
-        console.error("Error fetching question data:", error);
-      } finally {
-        setLoading(false);
-      }
+      setQuestionData(data);
+    } catch (error) {
+      console.error("Error fetching question data:", error);
+    } finally {
+      setLoading(false);
     }
+  }
+
+
+  useEffect(() => {
 
     if (params.questionid) {
       fetchQuestion();
@@ -62,10 +59,7 @@ export default function Page({ params }: { params: { questionid: string } }) {
       </Panel>
       <PanelResizeHandle style={{ width: "0.5rem", backgroundColor: "blue" }} />
       <Panel defaultSize={60} minSize={20} style={{height:'100vh'}}>
-        <CodeEditor
-          initialCode={questionData.driver_code}
-          onChange={handleCodeChange}
-        />
+        <CodeEditor questionName={params.questionid}/>
       </Panel>
     </PanelGroup>
   );
