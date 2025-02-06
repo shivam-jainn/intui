d463b47e2d7b12e69127e925e1ae01f64edd6dd9
shivam-jainn <shivam.clgstash@gmail.com>
2025-02-06 18:16:42 +0530

    feat: :sparkles: Implement authentication system with better-auth integration and user management

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { Paper, Title, Text } from "@mantine/core";
+import classes from "./auth.module.css";
+import SocialSignIn from "@/components/Auth/SocialSignIn";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={classes.wrapper}>
+      <Paper className={classes.form} radius={0} p={30}>
+        <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+          Welcome to Mantine Auth!
+        </Title>
+        
+        {children} {/* Login or Signup Form will be injected here */}
+        
+        <SocialSignIn /> 
+        
+       
+      </Paper>
+    </div>
+  );
+}
