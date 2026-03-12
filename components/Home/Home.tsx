e443a304ca76725f08a6f73578635638a9f0a0e8
shivam-jainn <shivam.clgstash@gmail.com>
2026-03-12 10:07:02 +0530

    feat: implement session-based rendering in Home component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,26 @@
+'use client';
+
 import React from 'react'
 import MixerHome from '../Mixer/MixerHome'
+import Landing from '../Landing/Landing'
+import { useSession } from '@/lib/auth-client'
+import { Skeleton } from '@mantine/core'
 
 export default function Home() {
+  const { data: session, isPending } = useSession()
+
+  if (isPending) {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <Skeleton height={400} radius="xl" mb="md" />
+        <Skeleton height={200} radius="xl" />
+      </div>
+    )
+  }
 
   return (
     <div>
-      <MixerHome />
+      {session ? <MixerHome /> : <Landing />}
     </div>
   )
 }
