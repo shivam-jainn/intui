928a4ab8dc33a47cc081aba8d9b57bb0d07dfa50
shivam-jainn <shivam.clgstash@gmail.com>
2026-06-30 00:08:14 +0530

    test : no redirect

--- app/p0/page.tsx
+++ app/p0/page.tsx
@@ -5,13 +5,13 @@ import { auth } from '@/lib/auth';
 import P0SimulationHome from '@/components/P0Simulation/P0SimulationHome';
 
 export default async function P0Page() {
-  const session = await auth.api.getSession({
-    headers: headers(),
-  });
+  // const session = await auth.api.getSession({
+  //   headers: headers(),
+  // });
 
-  if (!session?.user) {
-    redirect('/signin');
-  }
+  // if (!session?.user) {
+  //   redirect('/signin');
+  // }
 
   return <P0SimulationHome />;
 }
